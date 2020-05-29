90257115be801b10449a60234de08bd31becf32f
Daanish Rasheed <daanishrasheed@gmail.com>
2020-05-29 10:45:09 -0400

    few changes

--- knn.py
+++ knn.py
@@ -1,7 +1,6 @@
 import numpy as np
 import operator
 import pandas as pd
-from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.datasets import load_iris
